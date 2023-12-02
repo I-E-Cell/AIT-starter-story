65e53afcc0a971b2a5c608d7d74afc636feadc6a
Mitesh2806 <miteshmaity_22519@aitpune.edu.in>
2023-12-02 12:32:45 +0530

    javascript

---
+++ components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div>
+            <u>
+                <li>home</li>
+                <li>home</li>
+                <li>home</li>
+            </u>
+        </div>
+    )
+}
+
+export default Navbar
