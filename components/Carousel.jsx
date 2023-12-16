8a01a31834aa3201859cd6daea9b61de8c81a883
Sagar-Sharma-7 <6969sagarsharma@gmail.com>
2023-12-17 00:26:04 +0530

    test commit - carousel

--- components/Carousel.jsx
+++ components/Carousel.jsx
@@ -1,91 +1,116 @@
-import React from 'react'
+import React from "react";
 
 function Carousel() {
   return (
     <div>
-        <div className="wrapper1 flex items-center justify-center h-[95vh]">
-            <div className="slider h-[200px] overflow-hidden relative w-[1000%] m-auto">
-                <div className="slide-track animate-[scroll_20s_linear_infinite] flex w-[calc(250px_*_14)]">
-                    <div class="animate-ping slide h-[100px] w-[160px]">
-                        <img src="public\prac-img\img1.jpeg" alt="" />
-                    </div>
-                    <div class="animate-ping slide h-[100px] w-[160px]">
-                        <img className='animate-ping w-[100px] h-[100px] rounded-[10px] border-2 border-solid border-[#bbbbbb] hover:cursor-pointer hover:animate-[ease-in-out] hover:scale-[2]' src="public\prac-img\img1.jpeg" alt="" />
-                    </div>
-                    <div class="slide h-[100px] w-[160px]">
-                        <img className='w-[100px] h-[100px] rounded-[10px] border-2 border-solid border-[#bbbbbb] hover:cursor-pointer hover:animate-[ease-in-out] hover:scale-[2]' src="public\prac-img\img1.jpeg" alt="" />
-                    </div>
-                    <div class="slide h-[100px] w-[160px]">
-                        <img className='w-[100px] h-[100px] rounded-[10px] border-2 border-solid border-[#bbbbbb] hover:cursor-pointer hover:animate-[ease-in-out] hover:scale-[2]' src="public\prac-img\img1.jpeg" alt="" />
-                    </div>
-                    <div class="slide h-[100px] w-[160px]">
-                        <img className='w-[100px] h-[100px] rounded-[10px] border-2 border-solid border-[#bbbbbb] hover:cursor-pointer hover:animate-[ease-in-out] hover:scale-[2]' src="public\prac-img\img1.jpeg" alt="" />
-                    </div>
-                    <div class="slide h-[100px] w-[160px]">
-                        <img className='w-[100px] h-[100px] rounded-[10px] border-2 border-solid border-[#bbbbbb] hover:cursor-pointer hover:animate-[ease-in-out] hover:scale-[2]' src="public\prac-img\img1.jpeg" alt="" />
-                    </div>
-                    <div class="slide h-[100px] w-[160px]">
-                        <img className='w-[100px] h-[100px] rounded-[10px] border-2 border-solid border-[#bbbbbb] hover:cursor-pointer hover:animate-[ease-in-out] hover:scale-[2]' src="public\prac-img\img1.jpeg" alt="" />
-                    </div>
-                    <div class="slide h-[100px] w-[160px]">
-                        <img className='w-[100px] h-[100px] rounded-[10px] border-2 border-solid border-[#bbbbbb] hover:cursor-pointer hover:animate-[ease-in-out] hover:scale-[2]' src="public\prac-img\img1.jpeg" alt="" />
-                    </div>
-                    <div class="slide h-[100px] w-[160px]">
-                        <img  className='w-[100px] h-[100px] rounded-[10px] border-2 border-solid border-[#bbbbbb] hover:cursor-pointer hover:animate-[ease-in-out] hover:scale-[2]' src="public\prac-img\img1.jpeg" alt="" />
-                    </div>
-                    <div class="slide h-[100px] w-[160px]">
-                        <img className='w-[100px] h-[100px] rounded-[10px] border-2 border-solid border-[#bbbbbb] hover:cursor-pointer hover:animate-[ease-in-out] hover:scale-[2]' src="public\prac-img\img1.jpeg" alt="" />
-                    </div>
-                    <div class="slide h-[100px] w-[160px]">
-                        <img className='w-[100px] h-[100px] rounded-[10px] border-2 border-solid border-[#bbbbbb] hover:cursor-pointer hover:animate-[ease-in-out] hover:scale-[2]' src="public\prac-img\img1.jpeg" alt="" />
-                    </div>
-                    <div class="slide h-[100px] w-[160px]">
-                        <img className='w-[100px] h-[100px] rounded-[10px] border-2 border-solid border-[#bbbbbb] hover:cursor-pointer hover:animate-[ease-in-out] hover:scale-[2]'  src="public\prac-img\img1.jpeg" alt="" />
-                    </div>
-                    <div class="slide h-[100px] w-[160px]">
-                        <img src="public\prac-img\img1.jpeg" alt="" />
-                    </div>
-                    <div class="slide h-[100px] w-[160px]">
-                        <img className='w-[100px] h-[100px] rounded-[10px] border-2 border-solid border-[#bbbbbb] hover:cursor-pointer hover:animate-[ease-in-out] hover:scale-[2]' src="public\prac-img\img1.jpeg" alt="" />
-                    </div>
-                    <div class="slide h-[100px] w-[160px]">
-                        <img className='w-[100px] h-[100px] rounded-[10px] border-2 border-solid border-[#bbbbbb] hover:cursor-pointer hover:animate-[ease-in-out] hover:scale-[2]' src="public\prac-img\img1.jpeg" alt="" />
-                    </div>
-                    <div class="slide h-[100px] w-[160px]">
-                        <img className='w-[100px] h-[100px] rounded-[10px] border-2 border-solid border-[#bbbbbb] hover:cursor-pointer hover:animate-[ease-in-out] hover:scale-[2]' src="public\prac-img\img1.jpeg" alt="" />
-                    </div>
-                    <div class="slide h-[100px] w-[160px]">
-                        <img className='w-[100px] h-[100px] rounded-[10px] border-2 border-solid border-[#bbbbbb] hover:cursor-pointer hover:animate-[ease-in-out] hover:scale-[2]' src="public\prac-img\img1.jpeg" alt="" />
-                    </div>
-                    <div class="slide h-[100px] w-[160px]">
-                        <img className='w-[100px] h-[100px] rounded-[10px] border-2 border-solid border-[#bbbbbb] hover:cursor-pointer hover:animate-[ease-in-out] hover:scale-[2]' src="public\prac-img\img1.jpeg" alt="" />
-                    </div>
-                    <div class="slide h-[100px] w-[160px]">
-                        <img className='w-[100px] h-[100px] rounded-[10px] border-2 border-solid border-[#bbbbbb] hover:cursor-pointer hover:animate-[ease-in-out] hover:scale-[2]' src="public\prac-img\img1.jpeg" alt="" />
-                    </div>
-                    <div class="slide h-[100px] w-[160px]">
-                        <img className='w-[100px] h-[100px] rounded-[10px] border-2 border-solid border-[#bbbbbb] hover:cursor-pointer hover:animate-[ease-in-out] hover:scale-[2]' src="public\prac-img\img1.jpeg" alt="" />
-                    </div>
-                    <div class="slide h-[100px] w-[160px]">
-                        <img  className='w-[100px] h-[100px] rounded-[10px] border-2 border-solid border-[#bbbbbb] hover:cursor-pointer hover:animate-[ease-in-out] hover:scale-[2]' src="public\prac-img\img1.jpeg" alt="" />
-                    </div>
-                    <div class="slide h-[100px] w-[160px]">
-                        <img className='w-[100px] h-[100px] rounded-[10px] border-2 border-solid border-[#bbbbbb] hover:cursor-pointer hover:animate-[ease-in-out] hover:scale-[2]' src="public\prac-img\img1.jpeg" alt="" />
-                    </div>
-                    <div class="slide h-[100px] w-[160px]">
-                        <img className='w-[100px] h-[100px] rounded-[10px] border-2 border-solid border-[#bbbbbb] hover:cursor-pointer hover:animate-[ease-in-out] hover:scale-[2]' src="public\prac-img\img1.jpeg" alt="" />
-                    </div>
-                    <div class="slide h-[100px] w-[160px]">
-                        <img className='w-[100px] h-[100px] rounded-[10px] border-2 border-solid border-[#bbbbbb] hover:cursor-pointer hover:animate-[ease-in-out] hover:scale-[2]'  src="public\prac-img\img1.jpeg" alt="" />
-                    </div>
-                    
-                </div>
+      <div className="wrapper1 flex items-center justify-center h-[95vh]">
+        <div className="slider h-[200px] overflow-hidden relative w-[1000%] m-auto">
+          {/* <div className="slide-track animate-[scroll_20s_linear_infinite] flex w-[calc(250px_*_14)]"> */}
+          <div className="slide-track flex w-[calc(250px_*_14)] animate-[scroll_5s_linear_infinite_alternate]">
+            <div class="slide h-[200px] w-[200px] border-black border-2 border-solid">
+              <img
+                className="w-[150px] h-[150px] rounded-[10px]  hover:cursor-pointer hover:animate-[ease-in-out] hover:scale-[2]"
+                src="prac-img\steve.jpg"
+                alt=""
+              />
             </div>
-            
+            <div class="slide h-[200px] w-[200px] border-black border-2 border-solid">
+              <img
+                className="w-[150px] h-[150px] rounded-[10px]  hover:cursor-pointer hover:animate-[ease-in-out] hover:scale-[2]"
+                src="prac-img\steve.jpg"
+                alt=""
+              />
+            </div>
+            <div class="slide h-[200px] w-[200px] border-black border-2 border-solid">
+              <img
+                className="w-[150px] h-[150px] rounded-[10px]  hover:cursor-pointer hover:animate-[ease-in-out] hover:scale-[2]"
+                src="prac-img\steve.jpg"
+                alt=""
+              />
+            </div>
+            <div class="slide h-[200px] w-[200px] border-black border-2 border-solid">
+              <img
+                className="w-[150px] h-[150px] rounded-[10px]  hover:cursor-pointer hover:animate-[ease-in-out] hover:scale-[2]"
+                src="prac-img\steve.jpg"
+                alt=""
+              />
+            </div>
+            <div class="slide h-[200px] w-[200px] border-black border-2 border-solid">
+              <img
+                className="w-[150px] h-[150px] rounded-[10px]  hover:cursor-pointer hover:animate-[ease-in-out] hover:scale-[2]"
+                src="prac-img\steve.jpg"
+                alt=""
+              />
+            </div>
+            <div class="slide h-[200px] w-[200px] border-black border-2 border-solid">
+              <img
+                className="w-[150px] h-[150px] rounded-[10px]  hover:cursor-pointer hover:animate-[ease-in-out] hover:scale-[2]"
+                src="prac-img\steve.jpg"
+                alt=""
+              />
+            </div>
+            <div class="slide h-[200px] w-[200px] border-black border-2 border-solid">
+              <img
+                className="w-[150px] h-[150px] rounded-[10px]  hover:cursor-pointer hover:animate-[ease-in-out] hover:scale-[2]"
+                src="prac-img\steve.jpg"
+                alt=""
+              />
+            </div>
+            <div class="slide h-[200px] w-[200px] border-black border-2 border-solid">
+              <img
+                className="w-[150px] h-[150px] rounded-[10px]  hover:cursor-pointer hover:animate-[ease-in-out] hover:scale-[2]"
+                src="prac-img\steve.jpg"
+                alt=""
+              />
+            </div>
+            <div class="slide h-[200px] w-[200px] border-black border-2 border-solid">
+              <img
+                className="w-[150px] h-[150px] rounded-[10px]  hover:cursor-pointer hover:animate-[ease-in-out] hover:scale-[2]"
+                src="prac-img\steve.jpg"
+                alt=""
+              />
+            </div>
+            <div class="slide h-[200px] w-[200px] border-black border-2 border-solid">
+              <img
+                className="w-[150px] h-[150px] rounded-[10px]  hover:cursor-pointer hover:animate-[ease-in-out] hover:scale-[2]"
+                src="prac-img\steve.jpg"
+                alt=""
+              />
+            </div>
+            <div class="slide h-[200px] w-[200px] border-black border-2 border-solid">
+              <img
+                className="w-[150px] h-[150px] rounded-[10px]  hover:cursor-pointer hover:animate-[ease-in-out] hover:scale-[2]"
+                src="prac-img\steve.jpg"
+                alt=""
+              />
+            </div>
+            <div class="slide h-[200px] w-[200px] border-black border-2 border-solid">
+              <img
+                className="w-[150px] h-[150px] rounded-[10px]  hover:cursor-pointer hover:animate-[ease-in-out] hover:scale-[2]"
+                src="prac-img\steve.jpg"
+                alt=""
+              />
+            </div>
+            <div class="slide h-[200px] w-[200px] border-black border-2 border-solid">
+              <img
+                className="w-[150px] h-[150px] rounded-[10px]  hover:cursor-pointer hover:animate-[ease-in-out] hover:scale-[2]"
+                src="prac-img\steve.jpg"
+                alt=""
+              />
+            </div>
+            <div class="slide h-[200px] w-[200px] border-black border-2 border-solid">
+              <img
+                className="w-[150px] h-[150px] rounded-[10px]  hover:cursor-pointer hover:animate-[ease-in-out] hover:scale-[2]"
+                src="prac-img\steve.jpg"
+                alt=""
+              />
+            </div>
+          </div>
         </div>
-    
+      </div>
+      {/* second carousel */}
     </div>
-  )
+  );
 }
 
-export default Carousel
+export default Carousel;
